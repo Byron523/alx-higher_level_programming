e39cf7ab34fd325917b3784a43b41c3ecf3df5ed
Byron523 <byrongumbo23@outlook.com>
2022-12-01 13:32:10 +0200

    add

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+
+size = len(argv)
+if (size - 1) == 0:
+    print("{} arguments.".format(size - 1))
+elif (size - 1) == 1:
+    print("{} argument: \n{}: {}".format(size - 1, size - 1, argv[1]))
+else:
+    print("{} arguments:".format(size - 1))
+    for i in range(size - 1):
+        print("{}: {}".format(i + 1, argv[i + 1]))
