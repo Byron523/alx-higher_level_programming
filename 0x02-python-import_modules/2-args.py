9ac5cc5aed71176d72be0c07c7090107891835a7
Byron523 <byrongumbo23@outlook.com>
2022-12-01 19:58:52 +0200

    major changes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-size = len(argv)
-if (size - 1) == 0:
-    print("{} arguments.".format(size - 1))
-elif (size - 1) == 1:
-    print("{} argument: \n{}: {}".format(size - 1, size - 1, argv[1]))
-else:
-    print("{} arguments:".format(size - 1))
-    for i in range(size - 1):
-        print("{}: {}".format(i + 1, argv[i + 1]))
+    size = len(argv)
+    if (size - 1) == 0:
+        print("{} arguments.".format(size - 1))
+    elif (size - 1) == 1:
+        print("{} argument: \n{}: {}".format(size - 1, size - 1, argv[1]))
+    else:
+        print("{} arguments:".format(size - 1))
+        for i in range(size - 1):
+            print("{}: {}".format(i + 1, argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-
-add = 0
-a = len(argv)
-for i in range(a - 1):
-    add = add + int(argv[i + 1])
-print("{}".format(add))
+if __name__ == "__main__":
+    from sys import argv
+    add = 0
+    a = len(argv)
+    for i in range(a - 1):
+        add = add + int(argv[i + 1])
+    print("{}".format(add))
