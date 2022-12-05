ba1108147814733d5592ad0d4414babfb46975ca
Byron523 <byrongumbo23@outlook.com>
2022-12-05 15:43:27 +0200

    indent errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-        from sys import argv
-        i = len(argv) - 1
+    from sys import argv
+    i = len(argv) - 1
 
-        if i == 0:
-                print("{} arguments.".format(i))
-        elif i == 1:
-                print("{} argument:".format(i))
-        else:
-                print("{} arguments:".format(i))
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
 
-        if i >= 1:
-                i = 0
-                for item in argv:
-                        if i != 0:
-                                print("{}: {}".format(i, item)
-                        i += 1
+    if i >= 1:
+        i = 0
+        for item in argv:
+            if i != 0:
+                print("{}: {}".format(i, item)
+                i += 1
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-        from sys import argv
-        add = 0
-        for i in argv:
-                if i != argv[0]:
-                        add = add + int(argv)
-        print("{}".format(add))
+    from sys import argv
+    add = 0
+    for i in argv:
+        if i != argv[0]:
+            add = add + int(argv)
+    print("{}".format(add))
