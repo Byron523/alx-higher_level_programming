2591dc18effc75bcd25fe75a402e8a5f7532e73a
Byron523 <byrongumbo23@outlook.com>
2023-01-07 13:45:48 +0200

    requirement fail

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""A class that prevents user from dynamically creating new instance attributes"""
+"""A class that prevents user from dynamically
+creating new instance attributes"""
+
+
 class LockedClass:
     __slots__ = ['first_name']
 
