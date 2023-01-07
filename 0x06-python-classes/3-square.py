8df158c9684fa9a46a1e2ccfcb551b0625627a44
Byron523 <byrongumbo23@outlook.com>
2023-01-07 22:24:59 +0200

    returns a square of a nuber

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-class Square():
-    """ Class Square that defines a square """
+"""
+A class called Square that has a pvt
+instance called size
+"""
+
+
+class Square:
+    """ A Square class that have size as pvt attr """
+
     def __init__(self, size=0):
-        if size < 0:
+        """ an init method that initialise the class """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        return (self.__size * self.__size)
+        """ Area is a method that computes an area of an input """
+        return (self.__size ** 2)
