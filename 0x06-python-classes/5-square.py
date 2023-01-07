1393974c1374e38edb096e509b77088733afb42c
Byron523 <byrongumbo23@outlook.com>
2023-01-07 22:46:30 +0200

    prints the square to stdout

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
-class Square():
+"""
+A class called Square that has a pvt
+instance called size
+"""
+
+
+class Square:
+    """ A Square class that have size as pvt attr """
+
     def __init__(self, size=0):
-        if size < 0:
-            raise ValueError('size must be >= 0')
+        """ an init method that initialise the class """
         self.__size = size
 
     @property
     def size(self):
+        """ Size is a property getter """
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """ set size of square """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        return (self.__size * self.__size)
+        """ Area is a method that computes an area of an input """
+        return (self.__size ** 2)
 
     def my_print(self):
-        x = self.__size
-        if x == 0:
+        """ A method that prints to stdout the square """
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#",end='')
             print()
-        else:
-            for i in range(x):
-                for j in range(x):
-                    print("#", end='')
-                print()
 
