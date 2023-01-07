c5e50deddb5b956c9b8aec593f8a3bde110c39a1
Byron523 <byrongumbo23@outlook.com>
2023-01-07 13:39:55 +0200

    advanced

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+"""A class that prevents user from dynamically creating new instance attributes"""
+class LockedClass:
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ Init method """
+		pass
