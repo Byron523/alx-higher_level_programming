407d580a5b999f7c6254b0e22bbf31e38d54aa4b
Byron523 <byrongumbo23@outlook.com>
2023-01-07 22:37:24 +0200

    another class

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-class Square():
+"""
+A class called Square that has a pvt
+instance called size
+"""
+
+
+class Square:
+    """ A Square class that have size as pvt attr """
+
     def __init__(self, size=0):
-        if size < 0:
-            raise ValueError('size must be >= 0')
+        """ an init method that initialise the class """
         self.__size = size
 
     @property
     def size(self):
+        """ Size is a property getter """
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """ set size of square """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        return (self.__size * self.__size)
+        """ Area is a method that computes an area of an input """
+        return (self.__size ** 2)
