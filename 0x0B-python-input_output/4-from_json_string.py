261813a0bf0c3a980f1e76ba94c2a70415bb8fdf
Byron523 <byrongumbo23@outlook.com>
2023-01-10 11:20:10 +0200

    jsn represantntion

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A module that returns an object repr by JSON """
+
+import json
+
+def from_json_string(my_str):
+    """
+
+    A function that represent py DT as JSON string
+    Args:
+    filename: my_str
+
+
+    """
+    return (json.loads(my_str))
