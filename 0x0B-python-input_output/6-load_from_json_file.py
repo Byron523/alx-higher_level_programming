ae90387d2a86fe836ea8f02f1ac42f20cec852d0
Byron523 <byrongumbo23@outlook.com>
2023-01-10 12:12:48 +0200

    load from json file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -15,5 +15,5 @@ def load_from_json_file(filename):
 
 
     """
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         return (json.load(f))
