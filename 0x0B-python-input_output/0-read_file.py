7695ba028fd9350bf2d93515c0f16e33e6b2ae76
Byron523 <byrongumbo23@outlook.com>
2023-01-10 09:58:49 +0200

    first input file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" A python module that has a function that reads data """
+
+def read_file(filename=""):
+    """ Function that reads from a file
+     Args:
+    filename: filename
+    """
+
+    with open(filename, 'r', encoding="utf-8") as out:
+        read_data = out.read()
+		print(read_data, end='')
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,4 @@
+##Python input And Output
+
+-> These programs, handles files.
+-> They read write and print file contents
