fbae064a93aa75fa941df41a8c27812afe013c14
Byron523 <byrongumbo23@outlook.com>
2023-01-11 10:52:19 +0200

    added another closing bracket

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -10,4 +10,4 @@ def add_attribute(a_class, name1, name2):
 
     if not hasattr(a_class, "__dict__"):
         raise TypeError("can't add new attribute")
-    setattr(a_class, name1, name2
+    setattr(a_class, name1, name2)
