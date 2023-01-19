653cf0c8c5b1c76e9fec2d89ebb4b5af2b222342
Byron523 <byrongumbo23@outlook.com>
2023-01-19 11:37:06 +0200

    added a str func

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,3 +94,8 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end='')
             print()
+
+    def __str__(self):
+        """ a str method that returns a rectangle in string format """
+        return ("[Rectangle] {} {}/{} - {}/{}".format(self.id, self.x, self.y,
+                                                      self.width, self.height))
