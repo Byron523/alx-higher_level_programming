89dbb17b5f8a1f56ce791defb690b49a20da963e
Byron523 <byrongumbo23@outlook.com>
2023-01-20 12:23:16 +0200

    from json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -48,7 +48,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """ A method that returns a list of json """
-        if json_strin is None or json_string == "[]":
+        if json_string is None or json_string == "[]":
             return ([])
         else:
             return (json.loads(json_string))
