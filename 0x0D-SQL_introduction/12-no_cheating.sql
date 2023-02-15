5aff39171a08cea1853748c7c854703c81c95a55
Byron523 <byrongumbo23@outlook.com>
2023-02-15 21:42:26 +0200

    half way there

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- Using Bobs name to update the sccore of Bob
+UPDATE second_table SET score = 10 WHERE name = "Bob";
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Insert values into the databases
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
